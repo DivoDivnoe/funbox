9eab01a075218fc77c94d203fbd06a474bc1583f
Andrey Ivanov <andrey-tm203@mail.ru>
2018-04-07 19:09:57 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const items = document.querySelectorAll('.catalog__item');
+
+  [...items].forEach(item => {
+    const product = item.querySelector('.product');
+    const buy = item.querySelector('.catalog__btn');
+
+    buy.addEventListener('click', evt => {
+      product.classList.add('product--selected');
+      initHoverHandlers();
+    });
+
+    product.addEventListener('click', evt => {
+      if (product.classList.contains('product--disabled')) return false;
+
+      if (product.classList.contains('product--selected')) {
+        removeHoverHandlers();
+
+        if (product.classList.contains('product--hover')) {
+          product.classList.remove('product--hover');
+        }
+      } else {
+        initHoverHandlers();
+      }
+
+      product.classList.toggle('product--selected');
+    });
+
+    const initHoverHandlers = () => {
+      product.addEventListener('mouseenter', productHoverHandler);
+      product.addEventListener('mouseleave', productEndHoverHandler);
+    };
+
+    const removeHoverHandlers = () => {
+      product.removeEventListener('mouseenter', productHoverHandler);
+      product.removeEventListener('mouseleave', productEndHoverHandler);
+    };
+
+    const productHoverHandler = () => {
+      product.classList.add('product--hover');
+    };
+    const productEndHoverHandler = () => {
+      product.classList.remove('product--hover');
+    };
+  });
+});
